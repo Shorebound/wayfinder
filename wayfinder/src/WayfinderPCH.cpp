84c24cd2941b6e96cc8973cef57d15c68f301557
Shorebound <mark@steelgrin.net>
2025-04-28 03:19:56 +1000

    Added PCH Handling and cleaned up shared library handling

---
+++ wayfinder/src/WayfinderPCH.cpp
@@ -0,0 +1,4 @@
+#include "WayfinderPCH.h"
+
+// This file is intentionally empty.
+// It's only purpose is to generate the precompiled header.
---
+++ wayfinder/src/WayfinderPCH.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// Standard Library
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+// External Libraries
+#include "raylib.h"
+#include "raymath.h"
+
+// Wayfinder Core Definitions
+#include "core/Export.h"
