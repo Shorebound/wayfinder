b2ec9ea02c2abeb425061e24b542db79505803c3
Shorebound <mark@steelgrin.net>
2025-04-30 19:50:12 +1000

    some architecture change

---
+++ sandbox/journey/src/JourneyApplication.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "application/Application.h"
+#include "application/EntryPoint.h"
+
+namespace Journey
+{
+    class JourneyApplication : public Wayfinder::Application
+    {
+    public:
+        JourneyApplication(const Config& config = {}) : Application(config) {}
+    };
+}
+
+Wayfinder::Application* Wayfinder::CreateApplication(const Wayfinder::Application::CommandLineArgs& args)
+{
+    auto config = Wayfinder::Application::Config{
+        .ScreenWidth = 1920,
+        .ScreenHeight = 1080,
+        .WindowTitle = "Journey Sandbox",
+        .VSync = false};
+    return new Journey::JourneyApplication(config);
+}
