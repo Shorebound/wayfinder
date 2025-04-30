b6d25cf7d20f32d05cb5d58a5910cf281baf6a44
Shorebound <mark@steelgrin.net>
2025-04-30 20:17:50 +1000

    changed entry point parameters

--- engine/wayfinder/src/application/EntryPoint.h
+++ engine/wayfinder/src/application/EntryPoint.h
@@ -3,11 +3,9 @@
 
 extern Wayfinder::Application* Wayfinder::CreateApplication(const Application::CommandLineArgs& args);
 
-#if !defined(WAYFINDER_CUSTOM_MAIN)
-int main(int argc, char** argv)
+int main(int argc, char* argv[]) 
 {
     auto* app = Wayfinder::CreateApplication({argc, argv});
     app->Run();
     delete app;
 }
-#endif
